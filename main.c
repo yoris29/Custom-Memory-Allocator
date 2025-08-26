dd8c7c2f067c138163a60c4b44fbc3484ebe9541
yoris29 <ydoumi29@gmail.com>
2025-08-26 15:12:23 +0100

    Justing testing the brk() and sbrk() functions

--- main.c
+++ main.c
@@ -1,4 +1,15 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
 
+void *allocate(int val) {
+    int *brk_point = (int *) sbrk(val);
+    return brk_point;
+}
 
+int main() {
+    int *breakPoint = allocate(60);
+    printf("%p", breakPoint);
+
+    return 0;
+}
