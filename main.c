6b73f41fc4c256f1187259567b29f748086c7df2
yoris29 <ydoumi29@gmail.com>
2025-08-26 15:49:41 +0100

    Added chunk metadata structure and word alignment for x64 architectures

--- main.c
+++ main.c
@@ -2,6 +2,18 @@
 #include <stdio.h>
 #include <unistd.h>
 
+typedef struct Block {
+    size_t size;
+    int free;
+    struct chunk *prev;
+    struct chunk *next;
+    void *data;
+} chunk;
+
+size_t align(size_t size) {
+    return (size + 7) & ~7;
+}
+
 void *allocate(int val) {
     int *brk_point = (int *) sbrk(val);
     return brk_point;
