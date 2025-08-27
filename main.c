5da984c5292d0794f3e6022d30c7570280ab044d
yoris29 <ydoumi29@gmail.com>
2025-08-27 15:07:42 +0100

    Added canMerge() and merge() functions for adjacent memory blocks in the heap

--- main.c
+++ main.c
@@ -5,8 +5,8 @@
 typedef struct Block {
     size_t size;
     int free;
-    struct chunk *prev;
-    struct chunk *next;
+    struct Block *prev;
+    struct Block *next;
     void *data;
 } chunk;
 
@@ -14,8 +14,44 @@ size_t align(size_t size) {
     return (size + 7) & ~7;
 }
 
+chunk *getChunk(void *data) {
+    return (chunk *) ((char*) data  - sizeof(chunk));
+}
+
+int canMerge(chunk *chk) {
+    chunk *next = chk->next;
+    if(next != NULL && next->free) {
+	return 1;
+    }
+    return 0;
+}
+
+void merge(chunk *chk) {
+    chunk *next = chk->next;
+
+    if(next == NULL || !chk->free) {
+	return;
+    }
+    
+	chk->size += next->size +  sizeof(chunk); 
+	chk->next = next->next;
+
+	if(chk->next) {
+		chk->next->prev = chk;
+	}
+}
+
+void free(void *data) {
+    chunk *currentChunk = getChunk(data);
+    currentChunk->free = 1;
+}
+
 void *allocate(int val) {
     int *brk_point = (int *) sbrk(val);
+    if(brk_point == (void *) - 1) {
+	printf("Can't allocate memory yakho :p");
+	return;
+    }
     return brk_point;
 }
 
